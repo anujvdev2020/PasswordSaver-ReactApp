35853739c7f63374acb1c106ddaa3b24959e1138
Mamta <mamtaverma61020@gmail.com>
2023-07-30 14:00:28 +0530

    changrs for delete and modal

--- src/useGetPassword.js
+++ src/useGetPassword.js
@@ -1,10 +1,22 @@
 import React, { useState, useEffect } from "react";
 
 const useGetPassowrd = () => {
+
+  
   const password = localStorage.getItem("password");
   const [passwords, setPasswords] = useState(JSON.parse(password));
 
-  return { passwords };
+
+
+  const handleDelete = (id) => {
+    const password = localStorage.getItem("password");
+    let newPassWrd = JSON.parse(password);
+    let updated = newPassWrd.filter((item) => item.id !== id);
+    localStorage.setItem("password", JSON.stringify(updated));
+    setPasswords(updated)
+  };
+
+  return { passwords,handleDelete };
 };
 
 export default useGetPassowrd;
